2e0b2236e8ef2794b00a29866ab50897113a5f27
emilyriehl <eriehl@jhu.edu>
2024-08-01 15:25:48 +0200

    Setup blueprint

--- lakefile.lean
+++ lakefile.lean
@@ -19,3 +19,9 @@ This can be customized with additional library configuration options.
 @[default_target]
 lean_lib «infinity-cosmos» where
   -- Add any library configuration options here
+
+require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
+
+meta if get_config? env = some "dev" then
+require «doc-gen4» from git
+  "https://github.com/leanprover/doc-gen4" @ "main"
