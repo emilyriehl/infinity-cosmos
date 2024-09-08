b4fbf738d0ceb63cdb13d97918aa54b1d9d076b9
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2024-09-09 01:55:41 +0200

    Fix everything

--- InfinityCosmos/Mathlib/AlgebraicTopology/SimplicialSet.lean
+++ InfinityCosmos/Mathlib/AlgebraicTopology/SimplicialSet.lean
@@ -3,7 +3,7 @@ Copyright (c) 2021 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johan Commelin, Scott Morrison, Adam Topaz
 -/
-import Mathlib.AlgebraicTopology.SimplicialObject
+import InfinityCosmos.Mathlib.AlgebraicTopology.SimplicialObject
 import Mathlib.CategoryTheory.Limits.Shapes.Types
 import Mathlib.CategoryTheory.Yoneda
 import Mathlib.Data.Fin.VecNotation
