23626e3dcc432a26d7e2769f53e030e283c19156
emilyriehl <eriehl@jhu.edu>
2024-09-15 18:13:05 -0400

    attempt to fix a circular import error

--- InfinityCosmos/Mathlib/AlgebraicTopology/SimplicialObject.lean
+++ InfinityCosmos/Mathlib/AlgebraicTopology/SimplicialObject.lean
@@ -3,7 +3,7 @@ Copyright (c) 2021 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johan Commelin, Scott Morrison, Adam Topaz
 -/
-import Mathlib.AlgebraicTopology.SimplexCategory
+import InfinityCosmos.Mathlib.AlgebraicTopology.SimplexCategory
 import Mathlib.CategoryTheory.Comma.Arrow
 import Mathlib.CategoryTheory.Limits.FunctorCategory.Basic
 import Mathlib.CategoryTheory.Opposites
