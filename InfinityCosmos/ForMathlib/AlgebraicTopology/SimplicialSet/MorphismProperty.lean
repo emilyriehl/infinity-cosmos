29797448dd19111bd39a9701abba58d46517a673
Nima Rasekh <nima.rasekh@uni-greifswald.de>
2024-11-09 10:57:16 +0100

    Update MorphismProperty.lean

--- InfinityCosmos/ForMathlib/AlgebraicTopology/SimplicialSet/MorphismProperty.lean
+++ InfinityCosmos/ForMathlib/AlgebraicTopology/SimplicialSet/MorphismProperty.lean
@@ -27,23 +27,34 @@ def trivialFibration : MorphismProperty SSet := fun _ _ p ↦ BoundaryInclusions
 
 end trivialFibration
 
+/-- Definition of isofibration via inner fibrations -/
 section isoFibration
 
+/-- Inductive definition of inner horn inclusions Λ[n, i] ⟶ Δ[n] 
+  by restricting general horn inclusions to 0 < i < n -/
 inductive InnerInclusion : {X Y : SSet} → (X ⟶ Y) → Prop
   | mk (n i : ℕ) (low : 0 < i) (high : i < n) : InnerInclusion (hornInclusion n i)
 
+/-- The class of inner horn inclusions as a MorphismProperty -/
 def InnerInclusions : MorphismProperty SSet := fun _ _ p ↦ InnerInclusion p
 
+/-- Inductive definition of being equal to the coherent iso -/
 inductive CoherentIsoInclusion : {X Y : SSet} → (X ⟶ Y) → Prop
   | mk : CoherentIsoInclusion (coherentIso.pt .zero)
 
+/-- The class of the inclusion into the coherent iso as a MorphismProperty -/
 def IsCoherentIso : MorphismProperty SSet := fun _ _ p ↦ CoherentIsoInclusion p
 
+/-- The union of inner horn inclusions and inclusion into coherent iso -/
 def InnerIsoInclusion {X Y : SSet} (p : X ⟶ Y) : Prop :=
   (InnerInclusions p) ∨ IsCoherentIso p
 
+/-- The union of the class of inner horn inclusions and the inclusion into coherent iso 
+  as a MorphismProperty -/
 def InnerIsoInclusions : MorphismProperty SSet := fun _ _ p ↦ InnerIsoInclusion p
 
+/-- Definition of isofibration: An isofibration of quasi-categories is a morphism that
+has the rlp wrt inner horn inclusions and the inclusion into the walking coherent iso. -/
 def isoFibration : MorphismProperty SSet := fun _ _ p ↦ InnerIsoInclusions.rlp p
 
 end isoFibration
