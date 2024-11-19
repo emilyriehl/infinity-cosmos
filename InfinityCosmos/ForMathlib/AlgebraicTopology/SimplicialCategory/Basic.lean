fedd52f627634475a30b913a129a678bece621c4
Emily Riehl <eriehl@jhu.edu>
2024-11-19 11:34:06 -0500

    replaced our old definition of simplicial category with mathlib's updated definition

--- InfinityCosmos/ForMathlib/AlgebraicTopology/SimplicialCategory/Basic.lean
+++ InfinityCosmos/ForMathlib/AlgebraicTopology/SimplicialCategory/Basic.lean
@@ -3,8 +3,7 @@ Copyright (c) 2024 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/
-import Mathlib.AlgebraicTopology.SimplicialSet.Monoidal
-import Mathlib.CategoryTheory.Enriched.Basic
+import Mathlib.AlgebraicTopology.SimplicialCategory.Basic
 import Mathlib.CategoryTheory.Closed.Cartesian
 import Mathlib.CategoryTheory.Closed.FunctorToTypes
 
@@ -39,90 +38,45 @@ open CategoryTheory Category MonoidalCategory Simplicial SSet
 
 namespace CategoryTheory
 
-variable (C : Type u) [Category.{v} C]
-
-/-- A simplicial category is a category `C` that is enriched over the
-category of simplicial sets in such a way that morphisms in
-`C` identify to the `0`-simplices of the enriched hom. -/
-class SimplicialCategory extends EnrichedCategory SSet.{v} C where
-  /-- morphisms identify to `0`-simplices of the enriched hom -/
-  homEquiv (K L : C) : (K ⟶ L) ≃ (𝟙_ SSet.{v} ⟶ EnrichedCategory.Hom K L)
-  homEquiv_id (K : C) : homEquiv K K (𝟙 K) = eId SSet K := by aesop_cat
-  homEquiv_comp {K L M : C} (f : K ⟶ L) (g : L ⟶ M) :
-    homEquiv K M (f ≫ g) = (λ_ _).inv ≫ (homEquiv K L f ⊗ homEquiv L M g) ≫
-      eComp SSet K L M := by aesop_cat
-
 namespace SimplicialCategory
-
-variable [SimplicialCategory C]
-
-variable {C}
-
-/-- Abbreviation for the enriched hom of a simplicial category. -/
-abbrev sHom (K L : C) : SSet.{v} := EnrichedCategory.Hom K L
-
-/-- Abbreviation for the enriched composition in a simplicial category. -/
-abbrev sHomComp (K L M : C) : sHom K L ⊗ sHom L M ⟶ sHom K M := eComp SSet K L M
-
-/-- The bijection `(K ⟶ L) ≃ sHom K L _[0]` for all objects `K` and `L`
-in a simplicial category. -/
-def homEquiv' (K L : C) : (K ⟶ L) ≃ sHom K L _[0] :=
-  (homEquiv K L).trans (sHom K L).unitHomEquiv
+variable {C : Type u} [Category.{v} C] [SimplicialCategory C]
 
 /-- The morphism `sHom K' L ⟶ sHom K L` induced by a morphism `K ⟶ K'`. -/
-noncomputable def sHomWhiskerRight {K K' : C} (f : K ⟶ K') (L : C) :
-    sHom K' L ⟶ sHom K L :=
-  (λ_ _).inv ≫ homEquiv K K' f ▷ _ ≫ sHomComp K K' L
+noncomputable abbrev sHomWhiskerRight {K K' : C} (f : K ⟶ K') (L : C) :
+    sHom K' L ⟶ sHom K L := eHomWhiskerRight SSet f L
 
 @[simp]
-lemma sHomWhiskerRight_id (K L : C) : sHomWhiskerRight (𝟙 K) L = 𝟙 _ := by
-  simp [sHomWhiskerRight, homEquiv_id]
+lemma sHomWhiskerRight_id (K L : C) : sHomWhiskerRight (𝟙 K) L = 𝟙 _ :=
+  eHomWhiskerRight_id _ K L
 
 @[simp, reassoc]
 lemma sHomWhiskerRight_comp {K K' K'' : C} (f : K ⟶ K') (f' : K' ⟶ K'') (L : C) :
-    sHomWhiskerRight (f ≫ f') L = sHomWhiskerRight f' L ≫ sHomWhiskerRight f L := by
-  dsimp [sHomWhiskerRight]
-  simp only [assoc, homEquiv_comp, comp_whiskerRight, leftUnitor_inv_whiskerRight, ← e_assoc']
-  rfl
+    sHomWhiskerRight (f ≫ f') L = sHomWhiskerRight f' L ≫ sHomWhiskerRight f L :=
+  eHomWhiskerRight_comp _ f f' L
 
 /-- The morphism `sHom K L ⟶ sHom K L'` induced by a morphism `L ⟶ L'`. -/
-noncomputable def sHomWhiskerLeft (K : C) {L L' : C} (g : L ⟶ L') :
-    sHom K L ⟶ sHom K L' :=
-  (ρ_ _).inv ≫ _ ◁ homEquiv L L' g ≫ sHomComp K L L'
+noncomputable abbrev sHomWhiskerLeft (K : C) {L L' : C} (g : L ⟶ L') :
+    sHom K L ⟶ sHom K L' := eHomWhiskerLeft SSet K g
 
 @[simp]
-lemma sHomWhiskerLeft_id (K L : C) : sHomWhiskerLeft K (𝟙 L) = 𝟙 _ := by
-  simp [sHomWhiskerLeft, homEquiv_id]
+lemma sHomWhiskerLeft_id (K L : C) : sHomWhiskerLeft K (𝟙 L) = 𝟙 _ :=
+  eHomWhiskerLeft_id _ _ _
 
 @[simp, reassoc]
 lemma sHomWhiskerLeft_comp (K : C) {L L' L'' : C} (g : L ⟶ L') (g' : L' ⟶ L'') :
-    sHomWhiskerLeft K (g ≫ g') = sHomWhiskerLeft K g ≫ sHomWhiskerLeft K g' := by
-  dsimp [sHomWhiskerLeft]
-  simp only [homEquiv_comp, MonoidalCategory.whiskerLeft_comp, assoc, ← e_assoc]
-  rfl
+    sHomWhiskerLeft K (g ≫ g') = sHomWhiskerLeft K g ≫ sHomWhiskerLeft K g' :=
+  eHomWhiskerLeft_comp _ _ _ _
 
 @[reassoc]
 lemma sHom_whisker_exchange {K K' L L' : C} (f : K ⟶ K') (g : L ⟶ L') :
     sHomWhiskerLeft K' g ≫ sHomWhiskerRight f L' =
-      sHomWhiskerRight f L ≫ sHomWhiskerLeft K g :=
-  ((ρ_ _).inv ≫ _ ◁ homEquiv L L' g ≫ (λ_ _).inv ≫ homEquiv K K' f ▷ _) ≫=
-    (e_assoc SSet.{v} K K' L L').symm
+      sHomWhiskerRight f L ≫ sHomWhiskerLeft K g := eHom_whisker_exchange _ f g
 
 attribute [local simp] sHom_whisker_exchange
 
-variable (C) in
-/-- The bifunctor `Cᵒᵖ ⥤ C ⥤ SSet.{v}` which sends `K : Cᵒᵖ` and `L : C` to `sHom K.unop L`. -/
-@[simps]
-noncomputable def sHomFunctor : Cᵒᵖ ⥤ C ⥤ SSet.{v} where
-  obj K :=
-    { obj := fun L => sHom K.unop L
-      map := fun φ => sHomWhiskerLeft K.unop φ }
-  map φ :=
-    { app := fun L => sHomWhiskerRight φ.unop L }
-
 noncomputable instance : SimplicialCategory SSet where
   toEnrichedCategory := inferInstanceAs (EnrichedCategory (_ ⥤ Type _) (_ ⥤ Type _))
-  homEquiv K L :=
+  homEquiv {K} {L} :=
     letI e : (K ⟶ L) ≃ (K ⊗ 𝟙_ SSet ⟶ L) :=
       ⟨fun f => (ρ_ _).hom ≫ f, fun f => (ρ_ _).inv ≫ f, by aesop_cat, by aesop_cat⟩
     e.trans (Functor.homObjEquiv _ _ _).symm |>.trans (Functor.functorHomEquiv K L (𝟙_ SSet)).symm
@@ -136,7 +90,6 @@ noncomputable instance : MonoidalClosed SSet where
   }
 
 noncomputable instance : SymmetricCategory SSet := inferInstance
--- instance : HasBinaryProducts SSet := by infer_instance
 
 /-- The monoidal structure given by the `ChosenFiniteProducts` has good definitional properties,
 like the following: -/
