2246edcc6d50f597688aba0d1bbdc30734e493c5
Matej Penciak <matej.penciak@gmail.com>
2024-11-21 10:22:44 -0500

    defined map3

--- InfinityCosmos/ForMathlib/AlgebraicTopology/SimplicialSet/Pushouts.lean
+++ InfinityCosmos/ForMathlib/AlgebraicTopology/SimplicialSet/Pushouts.lean
@@ -1,20 +1,21 @@
 import InfinityCosmos.ForMathlib.AlgebraicTopology.SimplicialSet.CoherentIso
+import Mathlib.CategoryTheory.Limits.FunctorCategory.Basic
 
 open CategoryTheory Simplicial
 
 universe u
 
-def map1 : (Δ[1] ⟶ Δ[2] : Type u) :=
+def chain12 : Δ[1] ⟶ Δ[2] :=
   SSet.yonedaEquiv Δ[2] [1] |>.invFun
    (SSet.standardSimplex.edge 2 1 2
-     (by simp only [Nat.reduceAdd, Fin.isValue, Fin.reduceLE]))
+     (by decide))
 
-def map2 : (Δ[1] ⟶ Δ[2] : Type u) :=
+def chain01 : Δ[1] ⟶ Δ[2] :=
   SSet.yonedaEquiv Δ[2] [1] |>.invFun
    (SSet.standardSimplex.edge 2 0 1
-     (by simp only [Nat.reduceAdd, Fin.isValue, Fin.reduceLE]))
+     (by decide))
 
-noncomputable def Sq := Limits.pushout map1 map2
+noncomputable def Sq := Limits.pushout chain12 chain01
 
 #check Limits.coprod.desc
 
@@ -25,7 +26,7 @@ noncomputable def map_on_components {C} [Category C] {X Y Z W : C} [HasColimits 
 #check CategoryTheory.opOp_obj
 #check SSet.standardSimplex.objEquiv [1] (Opposite.op [0]) |>.toFun (SSet.standardSimplex.const 1 0 (Opposite.op [0]))
 
-def asdfasdf {n m : ℕ} (map_on_fin : Fin (m + 1) →o Fin (n + 1)) : ([m] : SimplexCategory) ⟶ [n] := SimplexCategory.mkHom map_on_fin
+def simplexmap_from_fin {n m : ℕ} (map_on_fin : Fin (m + 1) →o Fin (n + 1)) : ([m] : SimplexCategory) ⟶ [n] := SimplexCategory.mkHom map_on_fin
 
 #check SSet.asOrderHom
 
@@ -37,27 +38,44 @@ def simplex_from_map {n m : ℕ} (map : ([m] : SimplexCategory) ⟶ [n]) : Δ[n]
   simp at map
   exact map
 
+#check SSet
+
 def map_from_simplex {n m : ℕ} (simplex : Δ[n] _[m]) : Δ[m] ⟶ Δ[n] := SSet.yonedaEquiv Δ[n] [m] |>.invFun simplex
 
 #check SSet.standardSimplex.const 0 0
 
 #check SSet.standardSimplex.prod'
 
-noncomputable def map3 : (Δ[1] ⨿ Δ[1] ⟶ Δ[0] ⨿ Δ[0] : Type u) := by
+#check OrderHom.const (Fin 1) (β := Fin 0) ⟨0, _⟩
+
+noncomputable def map00p00 : (Δ[1] ⨿ Δ[1] ⟶ Δ[0] ⨿ Δ[0] : Type u) := by
   refine map_on_components ?_ ?_
-  · sorry
-  · sorry
+  · refine map_from_simplex ?_
+    refine simplex_from_map ?_
+    refine simplexmap_from_fin ?_
+    exact OrderHom.const (Fin 2) ⟨0, by decide⟩
+  · refine map_from_simplex ?_
+    refine simplex_from_map ?_
+    refine simplexmap_from_fin ?_
+    exact OrderHom.const (Fin 2) ⟨0, by decide⟩
 
 #check Limits.pushout
+
+#check SSet.yonedaEquiv Sq [1] |>.invFun
 
 open Limits in
 noncomputable def map4 : (Δ[1] ⨿ Δ[1] ⟶ Sq : Type u) := by
   refine coprod.desc ?_ ?_
-  · unfold Sq
-    sorry
-  · sorry
+  · refine SSet.yonedaEquiv Sq [1] |>.invFun ?_
+    refine CategoryTheory.Limits.colimitObjIsoColimitCompEvaluation (Limits.span chain12 chain01) (Opposite.op [1]) |>.toEquiv |>.invFun ?_
 
-noncomputable def Iso' := Limits.pushout map3 map4
+
+    · refine SSet.yonedaEquiv Sq [1] |>.invFun ?_
+
+
+    sorry
+
+noncomputable def Iso' := Limits.pushout map00p00 map4
 
 #check Limits.coprod Δ[1] Δ[1]
 
@@ -69,3 +87,6 @@ open SimplexCategory
 
 #check SSet.yonedaEquiv
 #check SSet.standardSimplex.map_id
+
+#check Limits.cospan
+#check CategoryTheory.Limits.colimitObjIsoColimitCompEvaluation (Limits.cospan chain12 chain01) (Opposite.op [1]) |>.toEquiv |>.invFun
