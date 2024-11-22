87d4d4b956ab28843b36bd5f10dc93dc4711a1df
Emily Riehl <eriehl@jhu.edu>
2024-11-22 14:44:49 -0500

    Experiments with the definition of an oo-cosmos (#39)
    * experimental tweaks to the definition
    
    * seminar work in progress
    
    * one sorry away from a correct definition
    
    * fix instance issue
    
    * section tests
    
    * term renaming
    
    * work on updating cotensors
    
    * fixed broken code
    
    * bad hacky proof finished post seminar
    
    * redefined the redefinition to close the last sorry
    
    * fixed broken build caused by updates to simplicial category definition
    
    * suggestions from mario
    
    * more mario suggestions
    
    * incomplete attempt to implement zulip suggestions
    
    * forgot one
    
    * filled in the limit sorries and cut the old definition
    
    ---------
    
    Co-authored-by: dagurtomas <dagurtomas@gmail.com>

--- InfinityCosmos/ForMathlib/AlgebraicTopology/SimplicialCategory/Cotensors.lean
+++ InfinityCosmos/ForMathlib/AlgebraicTopology/SimplicialCategory/Cotensors.lean
@@ -1,13 +1,149 @@
 import InfinityCosmos.ForMathlib.AlgebraicTopology.SimplicialCategory.Basic
+import InfinityCosmos.ForMathlib.CategoryTheory.Enriched.Cotensors
 import Mathlib.CategoryTheory.Closed.Cartesian
-
-noncomputable section
 
 namespace CategoryTheory
 
 open SimplicialCategory MonoidalCategory BraidedCategory
 
 universe v v₁ v₂ u u₁ u₂
+
+-- This section specializes the general notion of cotensors to simplicial categories.
+section specialization
+
+namespace SimplicialCategory
+variable {K : Type u}[Category.{v} K][SimplicialCategory K]
+
+open Enriched
+
+/-- A specialization of the enriched category cotensor.-/
+abbrev Cotensor (U : SSet) (A : K) := Enriched.Cotensor U A
+
+noncomputable section
+-- Noncomputability because this depends on `SSet.instChosenFiniteProducts`.
+
+def cotensorPostcompose {U : SSet} {A B : K} (ua : Cotensor U A) (ub : Cotensor U B) (f : A ⟶ B) :
+    ua.obj ⟶ ub.obj :=
+  (eHomEquiv SSet).symm (eHomEquiv SSet f ≫ Cotensor.postcompose _ ua ub)
+
+def cotensorPrecompose {U V : SSet} {A : K} (ua : Cotensor U A) (va : Cotensor V A) (i : U ⟶ V) :
+    va.obj ⟶ ua.obj :=
+  (eHomEquiv SSet).symm (eHomEquiv SSet i ≫ Cotensor.EhomPrecompose _ ua va)
+
+lemma cotensorPostcompose_homEquiv {U : SSet} {A B : K} (ua : Cotensor U A) (ub : Cotensor U B)
+    (f : A ⟶ B) : eHomEquiv SSet (cotensorPostcompose ua ub f) =
+      eHomEquiv SSet f ≫ Cotensor.postcompose _ ua ub := by
+  unfold cotensorPostcompose; simp
+
+lemma cotensorPrecompose_homEquiv {U V : SSet} {A : K} (ua : Cotensor U A) (va : Cotensor V A)
+    (i : U ⟶ V) : eHomEquiv SSet (cotensorPrecompose ua va i) =
+      eHomEquiv SSet i ≫ Cotensor.EhomPrecompose _ ua va  := by
+  unfold cotensorPrecompose; simp
+
+theorem cotensor_local_bifunctoriality {U V : SSet} {A B : K}
+    (ua : Cotensor U A) (ub : Cotensor U B) (va : Cotensor V A) (vb : Cotensor V B)
+    (i : U ⟶ V) (f : A ⟶ B) :
+    (cotensorPostcompose va vb f) ≫ (cotensorPrecompose ub vb i) =
+      (cotensorPrecompose ua va i) ≫ (cotensorPostcompose ua ub f) := by
+  apply (eHomEquiv SSet).injective
+  rw [eHomEquiv_comp, eHomEquiv_comp]
+  have thm := Cotensor.post_pre_eq_pre_post _ ua ub va vb
+  have compeq := whisker_eq ((λ_ _).inv ≫ (eHomEquiv SSet i ⊗ eHomEquiv SSet f)) thm
+  rw [Category.assoc, ← tensor_comp_assoc] at compeq
+  rw [← cotensorPostcompose_homEquiv, ← cotensorPrecompose_homEquiv] at compeq
+  rw [compeq]
+  slice_rhs 2 3 => rw [← tensor_comp, ← cotensorPostcompose_homEquiv, ← cotensorPrecompose_homEquiv]
+  simp only [braiding_naturality, braiding_tensorUnit_right, Category.assoc,
+    Iso.cancel_iso_inv_left]
+  monoidal
+
+end
+
+
+/-- `HasCotensor U A` represents the mere existence of a simplicial cotensor. -/
+class HasCotensor (U : SSet) (A : K) : Prop where mk' ::
+  /-- There is some cotensor. -/
+  exists_cotensor : Nonempty (Cotensor U A)
+
+theorem HasCotensor.mk {U : SSet} {A : K} (c : Cotensor U A) : HasCotensor U A :=
+  ⟨Nonempty.intro c⟩
+
+/-- Use the axiom of choice to extract explicit `CotensorCone A X` from `HasCotensor A X`. -/
+noncomputable def getCotensor (U : SSet) (A : K) [HasCotensor U A] : Cotensor U A :=
+  Classical.choice <| HasCotensor.exists_cotensor
+
+noncomputable section
+-- Interface to the `HasCotensor` class.
+
+/-- An arbitrary choice of cotensor obj. -/
+def cotensor.obj (U : SSet) (A : K) [HasCotensor U A] : K := (getCotensor U A).obj
+
+infixr:60 " ⋔ " => cotensor.obj
+
+/-- The associated cotensor cone. -/
+def cotensor.cone (U : SSet) (A : K) [HasCotensor U A] : U ⟶ sHom (U ⋔ A) A :=
+  (getCotensor U A).cone
+
+/-- The universal property of this cone.  -/
+def cotensor.is_cotensor (U : SSet) (A : K) [HasCotensor U A] : Cotensor U A := getCotensor U A
+
+/-- The natural isomorphism induced by a cotensor.-/
+def cotensor.iso (U : SSet) (A : K) [HasCotensor U A] (X : K) :
+    (sHom X (U ⋔ A)) ≅ ((ihom U).obj (sHom X A)) where
+      hom := Precotensor.coneNatTrans _ _
+      inv := (getCotensor U A).coneNatTransInv _
+      hom_inv_id := (getCotensor U A).NatTrans_NatTransInv_eq _
+      inv_hom_id := (getCotensor U A).NatTransInv_NatTrans_eq _
+
+def cotensor.iso.underlying (U : SSet) (A : K) [HasCotensor U A] (X : K) :
+  (X ⟶ (U ⋔ A)) ≃ (U ⟶ sHom X A) :=
+  (SimplicialCategory.homEquiv' X (U ⋔ A)).trans <|
+    (((evaluation SimplexCategoryᵒᵖ (Type _)).obj ⟨SimplexCategory.mk 0⟩).mapIso
+      (cotensor.iso U A X)).toEquiv.trans
+        (SimplicialCategory.homEquiv' U (sHom X A)).symm
+
+end
+
+variable (K) in
+-- DC: CotensoredCategory
+/-- `K` has simplicial cotensors when cotensors with any simplicial set exist. -/
+class HasCotensors : Prop where
+  /-- All `U : SSet` and `A : K` have a cotensor. -/
+  has_cotensors : ∀ U : SSet, ∀ A : K, HasCotensor U A := by infer_instance
+
+instance (priority := 100) hasCotensorsOfHasCotensors {K : Type u} [Category.{v} K]
+[SimplicialCategory K] [HasCotensors K] (U : SSet) (A : K) : HasCotensor U A :=
+  HasCotensors.has_cotensors U A
+
+section
+variable {K : Type u}[Category.{v} K][SimplicialCategory K][HasCotensors K]
+
+-- ER: I don't remember why I was considering an alternate form of this.
+noncomputable def cotensorCovMap (U : SSet) {A B : K} (f : A ⟶ B) : U ⋔ A ⟶ U ⋔ B :=
+  cotensorPostcompose _ _ f
+  -- (cotensor.iso.underlying U B (U ⋔ A)).symm ((cotensor.cone U A) ≫ (sHomWhiskerLeft (U ⋔ A) f))
+
+-- ER: I don't remember why I was considering an alternate form of this.
+noncomputable def cotensorContraMap {U V : SSet} (i : U ⟶ V) (A : K) : V ⋔ A ⟶ U ⋔ A :=
+  cotensorPrecompose _ _ i
+--  (cotensor.iso.underlying U A (V ⋔ A)).symm (i ≫ (cotensor.cone V A))
+
+-- DC: post_pre_eq_pre_post
+theorem cotensor_bifunctoriality {U V : SSet} (i : U ⟶ V) {A B : K} (f : A ⟶ B) :
+    (cotensorCovMap V f) ≫ (cotensorContraMap i B) =
+    (cotensorContraMap i A) ≫ (cotensorCovMap U f) := cotensor_local_bifunctoriality _ _ _ _ i f
+
+end
+
+
+end SimplicialCategory
+
+
+end specialization
+
+
+noncomputable section
+
 
 variable (K : Type u) [Category.{v} K]
 
@@ -36,78 +172,78 @@ structure CotensorCone (A : SSet) (X : K) where
   /-- The universal property of the limit cone -/
   is_cotensor : IsCotensor obj cone
 
-/-- `HasCotensor F` represents the mere existence of a simplicial cotensor. -/
-class HasCotensor (A : SSet) (X : K) : Prop where mk' ::
-  /-- There is some limit cone for `F` -/
-  exists_cotensor : Nonempty (CotensorCone A X)
+-- ER: Duplicated above.
+-- /-- `HasCotensor F` represents the mere existence of a simplicial cotensor. -/
+-- class HasCotensor (A : SSet) (X : K) : Prop where mk' ::
+--   /-- There is some limit cone for `F` -/
+--   exists_cotensor : Nonempty (CotensorCone A X)
 
-theorem HasCotensor.mk {A : SSet} {X : K} (c : CotensorCone A X) : HasCotensor A X :=
-  ⟨Nonempty.intro c⟩
+-- ER: Duplicated above.
+-- theorem HasCotensor.mk {A : SSet} {X : K} (c : CotensorCone A X) : HasCotensor A X :=
+--   ⟨Nonempty.intro c⟩
 
-/-- Use the axiom of choice to extract explicit `CotensorCone A X` from `HasCotensor A X`. -/
-def getCotensorCone (A : SSet) (X : K) [HasCotensor A X] : CotensorCone A X :=
-  Classical.choice <| HasCotensor.exists_cotensor
+-- ER: Duplicated above.
+-- /-- Use the axiom of choice to extract explicit `CotensorCone A X` from `HasCotensor A X`. -/
+-- def getCotensorCone (A : SSet) (X : K) [HasCotensor A X] : CotensorCone A X :=
+--   Classical.choice <| HasCotensor.exists_cotensor
 
-variable (K) in
--- DC: CotensoredCategory
-/-- `K` has simplicial cotensors when cotensors with any simplicial set exist. -/
-class HasCotensors : Prop where
-  /-- All `A : SSet` and `X : K` have a cotensor. -/
-  has_cotensors : ∀ A : SSet, ∀ X : K, HasCotensor A X := by infer_instance -- ER: I don't get what this means.
-
+-- ER: Duplicated above.
+-- variable (K) in
+-- -- DC: CotensoredCategory
+-- /-- `K` has simplicial cotensors when cotensors with any simplicial set exist. -/
+-- class HasCotensors : Prop where
+--   /-- All `A : SSet` and `X : K` have a cotensor. -/
+--   has_cotensors : ∀ A : SSet, ∀ X : K, HasCotensor A X := by infer_instance
 -- ER: copied; not sure what this is.
-instance (priority := 100) hasCotensorsOfHasCotensors {K : Type u} [Category.{v} K] [SimplicialCategory K] [HasCotensors K] (A : SSet) (X : K) : HasCotensor A X := HasCotensors.has_cotensors A X
 
--- Interface to the `HasCotensor` class.
-/-- An arbitrary choice of cotensor obj. -/
-def cotensor.obj (A : SSet) (X : K) [HasCotensor A X] : K :=
-  (getCotensorCone A X).obj
+-- instance (priority := 100) hasCotensorsOfHasCotensors {K : Type u} [Category.{v} K] [SimplicialCategory K] [HasCotensors K] (A : SSet) (X : K) : HasCotensor A X := HasCotensors.has_cotensors A X
 
-infixr:60 " ⋔ " => cotensor.obj
+-- ER: Duplicated above.
+-- -- Interface to the `HasCotensor` class.
+-- /-- An arbitrary choice of cotensor obj. -/
+-- -- def cotensor.obj (A : SSet) (X : K) [HasCotensor A X] : K :=
+-- --   (getCotensorCone A X).obj
 
-/-- The associated cotensor cone. -/
-def cotensor.cone (A : SSet) (X : K) [HasCotensor A X] : A ⟶ sHom (A ⋔ X) X :=
-  (getCotensorCone A X).cone
+-- -- infixr:60 " ⋔ " => cotensor.obj
 
-/-- The universal property of this cone.  -/
-def cotensor.is_cotensor (A : SSet) (X : K) [HasCotensor A X] :
-    IsCotensor (A ⋔ X) (cotensor.cone A X) := (getCotensorCone A X).is_cotensor
+-- /-- The associated cotensor cone. -/
+-- def cotensor.cone (A : SSet) (X : K) [HasCotensor A X] : A ⟶ sHom (A ⋔ X) X :=
+--   (getCotensorCone A X).cone
 
-def cotensor.iso (A : SSet) (X : K) [HasCotensor A X] (Y : K) :
-    -- Again the notation `A ⟶[SSet] sHom Y X` is ambiguous.
-    (sHom Y (A ⋔ X)) ≅ ((ihom A).obj (sHom Y X)) := by
-  have := (cotensor.is_cotensor A X).uniq Y
-  exact asIso (coneNatTrans Y (cone A X))
+-- /-- The universal property of this cone.  -/
+-- def cotensor.is_cotensor (A : SSet) (X : K) [HasCotensor A X] :
+--     IsCotensor (A ⋔ X) (cotensor.cone A X) := (getCotensorCone' A X).is_cotensor
 
-def cotensor.iso.underlying (A : SSet) (X : K) [HasCotensor A X] (Y : K) :
-  (Y ⟶ (A ⋔ X)) ≃ (A ⟶ sHom Y X) :=
-  (SimplicialCategory.homEquiv' Y (A ⋔ X)).trans <|
-    (((evaluation SimplexCategoryᵒᵖ (Type _)).obj ⟨SimplexCategory.mk 0⟩).mapIso
-      (cotensor.iso A X Y)).toEquiv.trans
-        (SimplicialCategory.homEquiv' A (sHom Y X)).symm
+-- def cotensor.iso (A : SSet) (X : K) [HasCotensor A X] (Y : K) :
+--     -- Again the notation `A ⟶[SSet] sHom Y X` is ambiguous.
+--     (sHom Y (A ⋔ X)) ≅ ((ihom A).obj (sHom Y X)) := by
+--   have := (cotensor.is_cotensor A X).uniq Y
+--   exact asIso (coneNatTrans Y (cone A X))
 
--- DC: postcompose
-def cotensorCovMap [HasCotensors K] (A : SSet) {X Y : K} (f : X ⟶ Y) : A ⋔ X ⟶ A ⋔ Y :=
-  (cotensor.iso.underlying A Y (A ⋔ X)).symm
-    ((cotensor.cone A X) ≫ (sHomWhiskerLeft (A ⋔ X) f))
+-- def cotensor.iso.underlying (A : SSet) (X : K) [HasCotensor A X] (Y : K) :
+--   (Y ⟶ (A ⋔ X)) ≃ (A ⟶ sHom Y X) :=
+--   (SimplicialCategory.homEquiv' Y (A ⋔ X)).trans <|
+--     (((evaluation SimplexCategoryᵒᵖ (Type _)).obj ⟨SimplexCategory.mk 0⟩).mapIso
+--       (cotensor.iso A X Y)).toEquiv.trans
+--         (SimplicialCategory.homEquiv' A (sHom Y X)).symm
 
--- DC: EhomPrecompose
-def cotensorContraMap [HasCotensors K] {A B : SSet} (i : A ⟶ B) (X : K) : B ⋔ X ⟶ A ⋔ X :=
-  (cotensor.iso.underlying A X (B ⋔ X)).symm (i ≫ (cotensor.cone B X))
+-- -- DC: postcompose
+-- def cotensorCovMap [HasCotensors K] (A : SSet) {X Y : K} (f : X ⟶ Y) : A ⋔ X ⟶ A ⋔ Y :=
+--   (cotensor.iso.underlying A Y (A ⋔ X)).symm
+--     ((cotensor.cone A X) ≫ (sHomWhiskerLeft (A ⋔ X) f))
 
--- DC: post_pre_eq_pre_post
-theorem cotensor_bifunctoriality [HasCotensors K] {A B : SSet} (i : A ⟶ B) {X Y : K} (f : X ⟶ Y) :
-    (cotensorCovMap B f) ≫ (cotensorContraMap i Y) =
-    (cotensorContraMap i X) ≫ (cotensorCovMap A f) := by sorry
+-- -- DC: EhomPrecompose
+-- def cotensorContraMap [HasCotensors K] {A B : SSet} (i : A ⟶ B) (X : K) : B ⋔ X ⟶ A ⋔ X :=
+--   (cotensor.iso.underlying A X (B ⋔ X)).symm (i ≫ (cotensor.cone B X))
 
--- def cotensor [SimplicialCategory K] : SSetᵒᵖ ⥤ K ⥤ K := sorry
+-- -- DC: post_pre_eq_pre_post
+-- theorem cotensor_bifunctoriality [HasCotensors K] {A B : SSet} (i : A ⟶ B) {X Y : K} (f : X ⟶ Y) :
+--     (cotensorCovMap B f) ≫ (cotensorContraMap i Y) =
+--     (cotensorContraMap i X) ≫ (cotensorCovMap A f) := by sorry
 
--- abbrev cotensorObj (A : SSet) (X : K) : K := (cotensor.obj (.op A)).obj X
-
--- infixr:60 " ⋔⋔ " => cotensorObj
 
 end SimplicialCategory
 
-end CategoryTheory
-
 end
+
+end CategoryTheory
--- InfinityCosmos/ForMathlib/InfinityCosmos/Basic.lean
+++ InfinityCosmos/ForMathlib/InfinityCosmos/Basic.lean
@@ -1,59 +1,107 @@
 import InfinityCosmos.ForMathlib.AlgebraicTopology.SimplicialCategory.Basic
 import InfinityCosmos.ForMathlib.AlgebraicTopology.SimplicialCategory.Cotensors
 import InfinityCosmos.ForMathlib.AlgebraicTopology.SimplicialCategory.Limits
+import InfinityCosmos.ForMathlib.AlgebraicTopology.SimplicialSet.MorphismProperty
 import Mathlib.AlgebraicTopology.SimplicialSet.Quasicategory
 import Mathlib.CategoryTheory.Closed.Cartesian
 import Mathlib.CategoryTheory.Limits.Shapes.Pullback.CommSq
 
 namespace CategoryTheory
 open Category Limits Functor MonoidalCategory Simplicial SimplicialCategory SSet
-universe v v₁ v₂ u u₁ u₂
+universe w v v₁ v₂ u u₁ u₂
 
 variable (K : Type u) [Category.{v} K]
 variable [SimplicialCategory K]
 
-class InfinityCosmos extends SimplicialCategory K where
-  IsIsoFibration {X Y : K} : (X ⟶ Y) → Prop
+
+/-- A `PreInfinityCosmos` is a simplicially enriched category whose hom-spaces are quasi-categories
+and whose morphisms come equipped with a special class of isofibrations.-/
+class PreInfinityCosmos extends SimplicialCategory K where
   [has_qcat_homs : ∀ {X Y : K}, SSet.Quasicategory (EnrichedCategory.Hom X Y)]
-  comp_isIsoFibration {X Y Z : K} {f : X ⟶ Y} {g : Y ⟶ Z} :
-    IsIsoFibration f → IsIsoFibration g → IsIsoFibration (f ≫ g)
-  iso_isIsoFibration {X Y : K} (e : X ≅ Y) : IsIsoFibration e.hom
-  has_terminal : HasTerminal K
+  IsIsoFibration : MorphismProperty K
+
+namespace InfinityCosmos
+
+variable {K : Type u} [Category.{v} K] [PreInfinityCosmos.{v} K]
+
+open PreInfinityCosmos
+
+/-- Common notation for the hom-spaces in a pre-∞-cosmos.-/
+def Fun (A B : K) : QCat where
+  obj := EnrichedCategory.Hom A B
+  property := PreInfinityCosmos.has_qcat_homs (X := A) (Y := B)
+
+noncomputable def representableMap' {X A B : K} (f : 𝟙_ SSet ⟶ EnrichedCategory.Hom A B) :
+    (EnrichedCategory.Hom X A : SSet) ⟶ (EnrichedCategory.Hom X B) :=
+  (ρ_ _).inv ≫ _ ◁ f ≫ EnrichedCategory.comp (V := SSet) X A B
+
+noncomputable def representableMap (X : K) {A B : K} (f : A ⟶ B) :
+    (EnrichedCategory.Hom X A : SSet) ⟶ (EnrichedCategory.Hom X B) :=
+  representableMap' (eHomEquiv SSet f)
+
+noncomputable def toFunMap (X : K) {A B : K} (f : A ⟶ B) :
+    Fun X A ⟶ Fun X B := representableMap X f
+
+-- Arguments of this type have the form `⟨ f hf ⟩`
+def IsoFibration (X Y : K) : Type v := {f : X ⟶ Y // IsIsoFibration f}
+
+-- Type with "\rr".
+infixr:25  " ↠ " => IsoFibration
+
+instance (A B : K) : Coe (A ↠ B) (A ⟶ B) := ⟨ λ f ↦ f.1 ⟩
+
+end InfinityCosmos
+
+open InfinityCosmos
+variable (K : Type u) [Category.{v} K][SimplicialCategory K] [PreInfinityCosmos.{v} K]
+
+/-- Experimenting with some changes.-/
+class InfinityCosmos extends PreInfinityCosmos K where
+  comp_isIsoFibration {X Y Z : K} (f : X ↠ Y) (g : Y ↠ Z) : IsIsoFibration (f.1 ≫ g.1)
+  iso_isIsoFibration {X Y : K} (e : X ⟶ Y) [IsIso e] : IsIsoFibration e
   all_objects_fibrant {X Y : K} (hY : IsTerminal Y) (f : X ⟶ Y) : IsIsoFibration f
-  has_products : HasBinaryProducts K -- TODO: replace by HasConicalProducts
-  prod_map_fibrant {X Y X' Y' : K} {f : X ⟶ Y} {g : X' ⟶ Y'} :
-    IsIsoFibration f → IsIsoFibration g → IsIsoFibration (prod.map f g)
-  [has_isoFibration_pullbacks {X Y Z : K} (f : X ⟶ Y) (g : Z ⟶ Y) :
-    IsIsoFibration g → HasPullback f g]
-  pullback_is_isoFibration {X Y Z P : K} (f : X ⟶ Z) (g : Y ⟶ Z)
-    (fst : P ⟶ X) (snd : P ⟶ Y) (h : IsPullback fst snd f g) :
-    IsIsoFibration g → IsIsoFibration fst
-  has_limits_of_towers (F : ℕᵒᵖ ⥤ K) :
-    (∀ n : ℕ, IsIsoFibration (F.map (homOfLE (Nat.le_succ n)).op)) → HasLimit F
+        -- TODO: replace by IsConicalTerminal?
+  [has_products : HasConicalProducts K]
+  prod_map_fibrant {γ : Type w} {A B : γ → K} (f : ∀ i, A i ↠ B i) :
+    IsIsoFibration (Limits.Pi.map (λ i ↦ (f i).1))
+  [has_isoFibration_pullbacks {A B E : K} (f : A ⟶ B) (g : E ↠ B) : HasConicalPullback f g.1]
+  pullback_is_isoFibration {A B E P : K} (f : A ⟶ B) (g : E ↠ B)
+    (fst : P ⟶ A) (snd : P ⟶ E) (h : IsPullback fst snd f g.1) : IsIsoFibration fst
+  [has_limits_of_towers (F : ℕᵒᵖ ⥤ K) :
+    (∀ n : ℕ, IsIsoFibration (F.map (homOfLE (Nat.le_succ n)).op)) → HasConicalLimit F]
   has_limits_of_towers_isIsoFibration (F : ℕᵒᵖ ⥤ K) (hf) :
     haveI := has_limits_of_towers F hf
     IsIsoFibration (limit.π F (.op 0))
-  [has_cotensors : HasCotensors K] -- ER: Added
-  -- leibniz_cotensor {X Y : K} (f : X ⟶ Y) (A B : SSet) (i : A ⟶ B) [Mono i]
-  --   (hf : IsIsoFibration f) {P : K} (fst : P ⟶ B ⋔⋔ Y) (snd : P ⟶ A ⋔⋔ X)
-  --   (h : IsPullback fst snd ((cotensor.map (.op i)).app Y) ((cotensor.obj (.op A)).map f)) :
-  --   IsIsoFibration (h.isLimit.lift <|
-  --     PullbackCone.mk ((cotensor.obj (.op B)).map f) ((cotensor.map (.op i)).app X) (by aesop_cat))
-  leibniz_cotensor {X Y : K} (f : X ⟶ Y) (A B : SSet) (i : A ⟶ B) [Mono i]
-    (hf : IsIsoFibration f) {P : K} (fst : P ⟶ B ⋔ Y) (snd : P ⟶ A ⋔ X)
-    (h : IsPullback fst snd (cotensorContraMap i Y) (cotensorCovMap A f)) :
+  [has_cotensors : HasCotensors K]
+  leibniz_cotensor  {U V : SSet} (i : U ⟶ V) [Mono i] {A B : K} (f : A ↠ B) {P : K}
+    (fst : P ⟶ V ⋔ B) (snd : P ⟶ U ⋔ A)
+    (h : IsPullback fst snd (cotensorContraMap i B) (cotensorCovMap U f)) :
     IsIsoFibration (h.isLimit.lift <|
-      PullbackCone.mk (cotensorCovMap B f) (cotensorContraMap i X) (cotensor_bifunctoriality i f))
-  local_isoFibration {X Y Z : K} (f : Y ⟶ Z) (hf : IsIsoFibration f) :
-    -- FIXME (V := SSet)
-    ∀ (_ : MonoidalClosed.curry (EnrichedCategory.comp (V := SSet) X Y Z) = sorry), sorry
-namespace InfinityCosmos
-variable [InfinityCosmos.{v} K]
+      PullbackCone.mk (cotensorCovMap V f.1) (cotensorContraMap i A)
+        (cotensor_bifunctoriality i f.1)) --TODO : Prove that these pullbacks exist.
+  local_isoFibration {X A B : K} (f : A ↠ B) : IsoFibration (toFunMap X f.1)
 
-def IsoFibration (X Y : K) : Type v := {f : X ⟶ Y // IsIsoFibration f}
+open InfinityCosmos
 
-infixr:25  " ↠ " => IsoFibration
+attribute [instance] has_products has_isoFibration_pullbacks has_limits_of_towers has_cotensors
 
-end InfinityCosmos
+section tests
+variable {K : Type u} [Category.{v} K] [InfinityCosmos K]
+
+open InfinityCosmos PreInfinityCosmos
+
+instance : HasConicalTerminal K := by infer_instance
+
+instance : HasTerminal K := by infer_instance
+
+instance : HasProducts K := by infer_instance
+
+theorem binary_prod_map_fibrant {X Y X' Y' : K} {f : X ↠ Y} {g : X' ↠ Y'} :
+    IsIsoFibration (prod.map f.1 g.1) := sorry
+
+def compIsofibration {A B C : K} (f : A ↠ B) (g : B ↠ C) : A ↠ C :=
+  ⟨(f.1 ≫ g.1), comp_isIsoFibration f g⟩
+
+end tests
 
 end CategoryTheory
