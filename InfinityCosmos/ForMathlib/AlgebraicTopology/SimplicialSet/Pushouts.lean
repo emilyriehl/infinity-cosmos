2ea077424d58317238681660218ca1233f1b6b89
Matej Penciak <matej.penciak@gmail.com>
2024-12-05 10:07:25 -0500

    got both equivs

--- InfinityCosmos/ForMathlib/AlgebraicTopology/SimplicialSet/Pushouts.lean
+++ InfinityCosmos/ForMathlib/AlgebraicTopology/SimplicialSet/Pushouts.lean
@@ -31,15 +31,10 @@ def simplexmap_from_fin {n m : ℕ} (map_on_fin : Fin (m + 1) →o Fin (n + 1)) 
 
 #check SSet.asOrderHom
 
+#check ULift
 -- I would have expected this to be around , but also didn't find it.
 -- I think if it really merits a proof, it shouldn't be here and in fact could be a lemma?
-def simplex_from_map {n m : ℕ} (map : ([m] : SimplexCategory) ⟶ [n]) : Δ[n] _[m] := by
-  refine SSet.standardSimplex.objMk ?_
-  refine ?f.toOrdHom
-  unfold SimplexCategory.smallCategory at map
-  unfold SimplexCategory.Hom at map
-  simp at map
-  exact map
+def simplex_from_map {n m : ℕ} (map : ([m] : SimplexCategory) ⟶ [n]) : Δ[n] _[m] := .up map
 
 #check SSet
 
@@ -73,29 +68,8 @@ noncomputable def map00p00 : (Δ[1] ⨿ Δ[1] ⟶ Δ[0] ⨿ Δ[0] : Type u) := b
 #check Limits.colimit
 
 open Limits in
-noncomputable def map4 : (Δ[1] ⨿ Δ[1] ⟶ Sq : Type u) := by
-  refine coprod.desc ?_ ?_
-  · refine SSet.yonedaEquiv Sq [1] |>.invFun ?_
-    refine CategoryTheory.Limits.colimitObjIsoColimitCompEvaluation (Limits.span chain12 chain01) (Opposite.op [1]) |>.toEquiv |>.invFun ?_
-    -- Here I have a question: Shouldn't the definition of the colimit not entail inclusion
-    -- maps from end points of the diagram?
-    -- Using the explicit description via quotients is perfectly correct, however, not the most formal
-    -- thing one could do here.
-    -- Concretely chain01 ≫ (Limits.pushout.inl chain12 chain01) is a map out of Δ[1] and we want to precompose
-    -- with its composition with evaluation.
-    let asdf := CategoryTheory.Limits.Types.colimitEquivQuot (span chain12 chain01 ⋙ evaluation SimplexCategoryᵒᵖ (Type u) _[1])
-    refine asdf.invFun ?_
-    apply Quot.mk
-    refine ⟨.some .left, ?_⟩
-    exact SSet.standardSimplex.edge 2 0 1 (by decide)
-
-  · refine SSet.yonedaEquiv Sq [1] |>.invFun ?_
-    refine CategoryTheory.Limits.colimitObjIsoColimitCompEvaluation (Limits.span chain12 chain01) (Opposite.op [1]) |>.toEquiv |>.invFun ?_
-    let asdf := CategoryTheory.Limits.Types.colimitEquivQuot (span chain12 chain01 ⋙ evaluation SimplexCategoryᵒᵖ (Type u) _[1])
-    refine asdf.invFun ?_
-    apply Quot.mk
-    refine ⟨.some .right, ?_⟩
-    exact SSet.standardSimplex.edge 2 1 2 (by decide)
+noncomputable def map4 : (Δ[1] ⨿ Δ[1] ⟶ Sq : Type u) :=
+  coprod.desc (chain01 ≫ (Limits.pushout.inl chain12 chain01)) (chain01 ≫ (Limits.pushout.inr chain12 chain01))
 
 def chain0 : Δ[0] ⟶ Δ[1] :=
   SSet.yonedaEquiv Δ[1] [0] |>.invFun
@@ -105,6 +79,12 @@ def chain1 : Δ[0] ⟶ Δ[1] :=
   SSet.yonedaEquiv Δ[1] [0] |>.invFun
    (SSet.standardSimplex.const 1 1 (Opposite.op [0]))
 
+noncomputable def firstEquiv := Limits.pushout map4 map00p00
+
+#check Limits.WidePullbackShape.wideCospan
+
+abbrev J := Fin 3
+
 noncomputable def Cospan00 := Limits.pushout chain0 chain0
 
 noncomputable def Cospan0011 := Limits.pushout (chain1 ≫ (Limits.pushout.inl chain0 chain0)) chain1
@@ -113,9 +93,35 @@ noncomputable def Cospan0011 := Limits.pushout (chain1 ≫ (Limits.pushout.inl c
 #check chain1 ≫ (Limits.pushout.inl chain0 chain0)
 
 -- Need to find an effective way to define this.
-noncomputable def map5 : (Cospan0011 ⟶ Δ[3] : Type u) := by sorry
+open Limits in
+noncomputable def map5 : (Cospan0011 ⟶ Δ[3] : Type u) := by
+  refine pushout.desc ?_ ?_ ?_
+  · refine pushout.desc ?_ ?_ ?_
+    · exact SSet.yonedaEquiv _ _ |>.invFun (SSet.standardSimplex.edge 3 1 2 (by decide))
+    · exact SSet.yonedaEquiv _ _ |>.invFun (SSet.standardSimplex.edge 3 1 3 (by decide))
+    · rfl
+  · exact SSet.yonedaEquiv _ _ |>.invFun (SSet.standardSimplex.edge 3 0 2 (by decide))
+  · ext
+    aesop_cat
 
-noncomputable def map6 : (Cospan0011 ⟶ Δ[1] : Type u) := by sorry
+open Limits in
+noncomputable def map6 : (Cospan0011 ⟶ Δ[1] : Type u) := by
+  refine pushout.desc ?_ ?_ ?_
+  · refine pushout.desc ?_ ?_ ?_
+    · exact SSet.yonedaEquiv _ _ |>.invFun (SSet.standardSimplex.edge 1 0 1 (by decide))
+    · exact SSet.yonedaEquiv _ _ |>.invFun (SSet.standardSimplex.edge 1 0 0 (by decide))
+    · rfl
+  · exact SSet.yonedaEquiv _ _ |>.invFun (SSet.standardSimplex.edge 1 1 1 (by decide))
+  · ext
+    aesop_cat
+
+noncomputable def secondEquiv := Limits.pushout map5 map6
+
+def someMap : secondEquiv ⟶ SSet.coherentIso := by sorry
+
+def someMap' : firstEquiv ⟶ secondEquiv := by sorry
+
+#check evaluation
 
 open Limits
 #check CategoryTheory.Limits.Types.colimitEquivQuot (span chain12 chain01 ⋙ evaluation SimplexCategoryᵒᵖ (Type u) _[1])
