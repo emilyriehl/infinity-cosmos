bc7fb39148f08724a8ddcefa2fa39acacd609239
Jon Eugster <eugster.jon@gmail.com>
2025-01-22 17:59:33 +0100

    update mathlib

--- InfinityCosmos/ForMathlib/CategoryTheory/Enriched/Ordinary/Limits/IsConicalLimit.lean
+++ InfinityCosmos/ForMathlib/CategoryTheory/Enriched/Ordinary/Limits/IsConicalLimit.lean
@@ -3,7 +3,7 @@ Copyright (c) 2025 Jon Eugster. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Dagur Asgeirsson, Jon Eugster, Emily Riehl
 -/
-import Mathlib.CategoryTheory.Enriched.Ordinary
+import Mathlib.CategoryTheory.Enriched.Ordinary.Basic
 import Mathlib.CategoryTheory.Limits.Preserves.Limits
 
 /-!
