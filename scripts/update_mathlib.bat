8b49715d2fb2f182920d44dbd045c6c2549f7731
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2025-02-16 09:48:45 +0100

    Delete unnecessary script

--- scripts/update_mathlib.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem Update Mathlib and the Lean toolchain.
-
-rem Download the latest lean-toolchain file from the Mathlib4 repository
-curl -L https://raw.githubusercontent.com/leanprover-community/mathlib4/master/lean-toolchain -o lean-toolchain
-
-rem Update the Mathlib dependencies and ensure doc-gen is also updated
-rem The `-Kenv=dev` flag ensures that the development environment is updated, including doc-gen
-lake -Kenv=dev update
